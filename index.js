139589c0f484a6e944145204d9214037a38a9947
Naiara Nuñez <naiaralihuel@gmail.com>
2023-01-31 11:03:02 -0300

    commit 1

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express")
-
 const app = express()
+
 app.use(express.static(__dirname + '/public'));
-
-app.get('/', (_,res) => {
-    res.send("index.html")
+app.get("/", (_,res) => {
+    res.send('index.html')
 })
-
 const PORT = 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor express escuchando en el puerto ${PORT}`)
