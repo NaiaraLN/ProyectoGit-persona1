9ebd92d6b9243d362f1f8e2564a0df49c3a4dfb6
Naiara Nuñez <naiaralihuel@gmail.com>
2023-01-31 10:50:54 -0300

    agrego gitignore

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (_,res) => {
+    res.send("index.html")
+})
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor express escuchando en el puerto ${PORT}`)
+  })
+  server.on('error', error => console.log(`Error en servidor: ${error}`))
